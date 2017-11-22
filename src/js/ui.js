741e24a4c1390bb9732fa77815a3c60b48d1ba71
Yura Lee <2youlol@gmail.com>
2017-11-22 18:44:58 +0900

    closest 폴리필 수정

--- src/js/ui.js
+++ src/js/ui.js
@@ -27,16 +27,18 @@ for(var i=0; i<$layerClose.length; i++){
 
 
 // 폴리필
-if (window.Element && !Element.prototype.closest) {
-    Element.prototype.closest =
-    function(s) {
-        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-            i,
-            el = this;
-        do {
-            i = matches.length;
-            while (--i >= 0 && matches.item(i) !== el) {};
-        } while ((i < 0) && (el = el.parentElement));
-        return el;
-    };
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+                                Element.prototype.webkitMatchesSelector;
+}
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+	    var el = this;
+	    if (!document.documentElement.contains(el)) return null;
+	    do {
+	        if (el.matches(s)) return el;
+	        el = el.parentElement;
+	    } while (el !== null);
+	    return null;
+	};
 }
